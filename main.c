7bce956deb3ec3688aeb5d3e5c2efc15187d1aa3
ProgrammerKos <kostyagusi@gmail.com>
2019-01-21 19:49:51 +0200

    Programm "X and Y" (virsion 2.0).
    Programm "X and Y" (virsion 2.0).

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <string.h>
-
 int main(){
-    float y;
-    int x;
+    float y; int x;
     for (int i = -5; i <= 10; i++){
         x = i;
         if (x == 0){
-            printf("0 : infinity\n");}
+        printf("0 : infinity\n");}
         else if (x < 5){
             y = x + 5;
             y = y / (x * x);
@@ -17,4 +13,4 @@ int main(){
             y = x * x;
             printf("%d : %f", x, y);
             printf("\n");}}
-    return 0;}
+return 0; }
